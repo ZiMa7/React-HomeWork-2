e6459b45bf3e8203a9e977564249151c29647170
Mariia <breslavec.marija@gmail.com>
2022-07-20 19:00:19 +0300

    Homework 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const LoginForm = ({ onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  console.log(password, username);
+  return (
+    <>
+      <div>
+        <span>User Name</span>
+        <input
+          className="user_name"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <span>Password</span>
+        <input
+          className="password"
+          type="text"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button
+        className="submit"
+        disabled={username.length > 5 && password.length > 8 ? false : true}
+        onClick={() => onLogin({ username, password })}
+      >
+        Submit
+      </button>
+    </>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginForm
+        onLogin={({ username, password }) =>
+          alert(`User Name: ${username}, Password: ${password}`)
+        }
+      />
     </div>
   );
 }
